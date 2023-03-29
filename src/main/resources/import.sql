3518f6f64e007b4fe4d1b52f6077988bbea96c28
gioanbatan <giovannibassani@hotmail.com>
2023-03-29 17:20:47 +0200

    Add: message for empty list of pizzas Fix: Names of ingredients in inport.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO `pizzas` (`name`, `description`, `price`) VALUES ('Margherita', 'Pomodoro - mozzarella', '6.00')
-INSERT INTO `pizzas` (`name`, `description`, `price`) VALUES ('Quattro formaggi', 'mozzarella - fontina - emmental - gorgonzola', '8.50')
-INSERT INTO `pizzas` (`name`, `description`, `price`) VALUES ('Diavola', 'pomodoro - mozzarella - salame piccante', '8.50')
+INSERT INTO `pizzas` (`name`, `description`, `price`) VALUES ('Quattro formaggi', 'Mozzarella - fontina - emmental - gorgonzola', '8.50')
+INSERT INTO `pizzas` (`name`, `description`, `price`) VALUES ('Diavola', 'Pomodoro - mozzarella - salame piccante', '8.50')
