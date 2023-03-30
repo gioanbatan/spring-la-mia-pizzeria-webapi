d0bb2f5b496bb219f117314ca88ca592aa3a7f0f
gioanbatan <giovannibassani@hotmail.com>
2023-03-30 17:05:29 +0200

    Add: show page for pizza

--- src/main/java/com/experis/pizza/controller/PizzaController.java
+++ src/main/java/com/experis/pizza/controller/PizzaController.java
@@ -3,12 +3,16 @@ package com.experis.pizza.controller;
 import com.experis.pizza.model.Pizza;
 import com.experis.pizza.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -21,5 +25,16 @@ public class PizzaController {
         List<Pizza> pizzas = pizzaRepository.findAll();
         model.addAttribute("list", pizzas);
         return "/pizzas/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("pizza", result.get());
+            return "/pizzas/show";
+        } else {
+            throw new ResponseStatusException((HttpStatus.NOT_FOUND));
+        }
     }
 }
